0bedb4d06b103d6bfdb74641f282ad3c8626087d
Atul Kumar <atuljaiswal.se@gmail.com>
2022-11-29 00:17:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+   <>
+    <h1>Fewwork Dummy</h1>
+    <h2>This is for trial only </h2>
+    <h3>Team collabration</h3>
+   </>
+  );
+}
+
+export default App;
